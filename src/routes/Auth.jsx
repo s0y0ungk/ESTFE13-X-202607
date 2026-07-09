05c09e277aa20a96c340aa445f342f474a4ab2f0
s0y0ungk <soyo2039@gmail.com>
2026-07-09 11:30:30 +0900

    feat: implement authentication form with email and password handling

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,12 +1,71 @@
 import { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Typography, TextField, Button } from "@mui/material";
+import { authService } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Auth() {
   const [newAccount, setNewAccount] = useState(true);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const auth = authService;
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setForm(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (newAccount) {
+      //회원가입
+      createUserWithEmailAndPassword(auth, form.email, form.password)
+        .then(userCredential => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+        })
+        .catch(error => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    } else {
+      //로그인
+    }
+  };
   return (
     <>
-      <h2>Auth</h2>
-      <Box component="form"></Box>
+      <Typography variant="h2" component="h2">
+        {newAccount ? "회원가입 폼" : "로그인 폼"}
+      </Typography>
+      <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
+        <TextField
+          fullWidth
+          label="Email address"
+          type="text"
+          name="email"
+          variant="outlined"
+          onChange={handleChange}
+        />
+        <TextField
+          sx={{ mt: 2 }}
+          fullWidth
+          label="Password"
+          type="password"
+          name="password"
+          variant="outlined"
+          onChange={handleChange}
+        />
+        <Button sx={{ mt: 2 }} type="submit" variant="contained">
+          {newAccount ? "회원가입" : "로그인"}
+        </Button>
+      </Box>
     </>
   );
 }
